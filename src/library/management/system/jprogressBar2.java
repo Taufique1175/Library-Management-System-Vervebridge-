00c49c543531f1441732c74940af77be6e3c4c86
Md Taufique Umar <mdtaufiqueumar637@gmail.com>
2024-07-08 23:09:43 +0530

    Inital commit

---
+++ src/library/management/system/jprogressBar2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library.management.system;
+
+/**
+ *
+ * @author ASUS
+ */
+class jprogressBar2 {
+
+    static void setValue(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
